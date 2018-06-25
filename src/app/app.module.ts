33740d31a3d771d439614f25afc92bb2f371bdaa
Broggoli <broggoli.nb@gmail.com>
2018-06-25 20:49:06 +0200

    started the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AlertComponent } from './alert/alert.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AlertComponent,
+    HomeComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+          path: "",
+          component: HomeComponent
+
+      },
+      {
+          path: "login",
+          component: LoginComponent
+
+      },
+      {
+          path: "admin",
+          component: AdminComponent
+
+      }])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  loginUser(event){
+    event.preventDefault()
+    const target = event.target
+    //getting the text out of the form
+    const username = target.querySelector("#username").value
+    const password = target.querySelector("#password").value
+
+    this.Auth.getUserDetails(username, password)
+    console.log(username, password)
   }
 
 }
